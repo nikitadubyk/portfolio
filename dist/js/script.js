27ee6a56238082bc341818e98d47a88d57f470d9
Nikita <nikita_dubyk@mail.ru>
2020-12-12 16:27:06 +0300

    add phpmailer

--- dist/js/script.js
+++ dist/js/script.js
@@ -17,3 +17,30 @@ const counters = document.querySelectorAll('.skills__item-percent'),
 counters.forEach( (item, i) => {
     lines[i].style.width = item.innerHTML;
 });
+
+
+// плавный скролл
+
+var $page = $('html, body');
+$('a[href*="#"]').click(function() {
+    $page.animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 400);
+    return false;
+});
+
+
+
+//phpmailer
+$('form').submit(function(e) {
+    e.preventDefault();
+    $.ajax({
+        type: "POST",
+        url: "mailer/smart.php",
+        data: $(this).serialize()
+    }).done(function() {
+        $(this).find("input").val("");
+    });
+    return false;
+});
+
