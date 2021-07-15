449ed2304a64c18cef7d6f114dda2b623860a382
Nikita <nikita_dubyk@mail.ru>
2021-07-15 22:14:58 +0300

    change text index.html, block scss, img at aside

--- dist/js/script.js
+++ dist/js/script.js
@@ -3,13 +3,29 @@ const menu = document.querySelector('.menu');
 const closeElem = document.querySelector('.menu__close');
 
 hamburger.addEventListener('click', () => {
-    menu.classList.add('active');
+    showModal();
 });
+
+function showModal() {
+    menu.classList.add('active');
+    document.body.style.overflow = 'hidden';
+}
+
+function closeModal() {
+    menu.classList.remove('active');
+    document.body.style.overflow = '';
+}
 
 closeElem.addEventListener('click', () => {
-    menu.classList.remove('active');
+    closeModal();
 });
 
+
+document.addEventListener('click', (e) => {
+    if (e.target.classList.contains('menu__overlay')) {
+        closeModal();
+    }
+});
 
 const counters = document.querySelectorAll('.skills__item-percent'),
     lines = document.querySelectorAll('.skills__item-scale span');
@@ -18,7 +34,6 @@ counters.forEach( (item, i) => {
     lines[i].style.width = item.innerHTML;
 });
 
-
 // плавный скролл
 
 var $page = $('html, body');
@@ -26,6 +41,7 @@ $('a[href*="#"]').click(function() {
     $page.animate({
         scrollTop: $($.attr(this, 'href')).offset().top
     }, 400);
+    closeModal();
     return false;
 });
 
